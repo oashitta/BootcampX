71641f2d7f76e30ce43e9f2cd52b1976cf334ffe
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-05-04 12:07:36 -0400

    adding parameterized queries to prevent SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,14 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts 
-ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+                    FROM students
+                    JOIN cohorts 
+                    ON cohorts.id = cohort_id
+                    WHERE cohorts.name LIKE $1
+                    LIMIT $2;`
+
+pool.query(queryString, values)
 .then(res => {
   // console.log(res.rows);
   res.rows.forEach(user => {
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers 
-JOIN assistance_requests ON teachers.id = teacher_id
-JOIN students on student_id = students.id 
-JOIN cohorts on cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
-ORDER BY teacher;
-`)
+const cohortName = process.argv[2] || 'JUL02';
+
+// Store all potentially malicious values in an array.
+const value = [`%${cohortName}%`];
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+                    FROM teachers 
+                    JOIN assistance_requests ON teachers.id = teacher_id
+                    JOIN students on student_id = students.id 
+                    JOIN cohorts on cohort_id = cohorts.id
+                    WHERE cohorts.name LIKE $1
+                    ORDER BY teacher;`
+
+pool.query(queryString, value)
 .then(res => {
   // console.log(res.rows);
   res.rows.forEach(user => {
